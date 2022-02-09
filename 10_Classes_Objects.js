d9ec51e4f0bc7c619a118875e6e9846890220e4e
AhmedRaza1211 <ahmedraza8325@gmail.com>
2022-02-09 22:27:40 +0500

    introduction of class

--- 10_Classes_Objects.js
+++ 10_Classes_Objects.js
@@ -1,10 +1,50 @@
-// Making CLASS
-class greeting {
-    morning() {
-        console.log('Salam Ahmed Raza Attari');
+// // Example # 1
+// // Making CLASS
+// class greeting {
+//     constructor() {
+//         let name;
+//         console.log('This is a Constructor Function');
+//     }
+
+//     whenYouCall() {
+//         console.log('Salam ' + this.name + ' bhai kia hal he');
+//     }
+// }
+
+// // Making OBJECT
+// let a = new greeting;
+// a.name = 'Ahmed Raza Attair';
+// a.whenYouCall();
+
+// // Example # 2
+class second {
+
+    // 1 # Constructor Function
+    // run by default
+    constructor(name, age) {
+        this.name = name,
+        this.age = age,
+        console.log('This is constructor function and it will run automatically');
     }
+
+    // 2 # Prototype 
+    // run when we call
+    whenCall() {
+        console.log(`Salam ${this.name} bhai kia hal he ap ka. I am ${this.age} years old`);
+    }
+
+    // 3 # Static Method
+    // call without object name, only call with (class name)
+    static functionNameShouldBeAnything() {
+        console.log('This is Statis Method');
+    }
+
 }
 
-// Making OBJECT
-let a = new greeting;
-a.morning();
+let one = new second('Ahmed Raza Attari', 23);
+one.whenCall()
+second.functionNameShouldBeAnything();
+
+let two = new second('Abdul Hadi', 24);
+two.whenCall();
+second.functionNameShouldBeAnything();
