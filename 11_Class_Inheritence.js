59de1ad666f0828edc5ec67ae8a74aaa37c51740
AhmedRaza1211 <ahmedraza8325@gmail.com>
2022-02-11 23:56:58 +0500

    class inheritence

--- 11_Class_Inheritence.js
+++ 11_Class_Inheritence.js
@@ -45,25 +45,61 @@
 // a.nameName();
 
 
-// Example # 3
-class peoplee {
-    constructor(name) {
-        this.name = name
-        console.log('Constructor Function: This is Peoplee Constructor');
+// // Example # 3
+// class peoplee {
+//     constructor(name) {
+//         this.name = name
+//         console.log('Constructor Function: This is Peoplee Constructor');
+//     }
+
+//     nameName() {
+//         console.log('People name is: ' + this.name);
+//     }
+// }
+
+// class teacherr extends peoplee {
+//     nameName() {
+//         // Now both (nameName) will run
+//         super.nameName();
+//         console.log('Teacher name is: ' + this.name);
+//     }
+// }
+
+// let aa = new teacherr('John Cena');
+// aa.nameName();
+
+
+// Example # 4
+class familyMember {
+    constructor(name, age, number) {
+        this.name = name;
+        this.age = age;
+        this.number = number;
+        console.log('Constructor: Family Members');
     }
 
-    nameName() {
-        console.log('People name is: ' + this.name);
+    details() {
+        document.write(`<h2> Niece Details </h2>
+                        Name: ${this.name} <br/>
+                        Age: ${this.age} <br/>
+                        Number: ${this.number} <br/>`);
+    }
+
+
+}
+
+class childs extends familyMember {
+    // constructor() {
+    //     super()
+    // }
+    details() {
+        let fastFood = 'Pizza';
+        let drink = 'Day Fresh';
+        let extras = fastFood + drink;
+
+        document.write(<h2> </h2>)
     }
 }
 
-class teacherr extends peoplee {
-    nameName() {
-        // Now both (nameName) will run
-        super.nameName();
-        console.log('Teacher name is: ' + this.name);
-    }
-}
-
-let aa = new teacherr('John Cena');
-aa.nameName();
+let s1 = new childs('Zainab', '2 years', 'Elder')
+s1.details();
